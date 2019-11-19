95245b40f0c0ac346a116950b61c21d87d2495eb
Benjamin Wohlwend <beni@elastic.co>
2019-11-19 11:42:15 +0100

    update requirements, apply black, and change perf to pyperf

--- run_bench.py
+++ run_bench.py
@@ -1,41 +1,42 @@
-import os
-import operator
 import importlib
+import operator
+import os
 import pkgutil
 
-import perf
-
 import benchmarks
+import pyperf
 
 
 def discover_benchmarks():
-    for importer, modname, is_pkg in sorted(pkgutil.iter_modules(benchmarks.__path__), key=operator.itemgetter(1)):
-        if modname.startswith('bm_'):
-            bench_module = importlib.import_module('benchmarks.' + modname)
+    for importer, modname, is_pkg in sorted(
+        pkgutil.iter_modules(benchmarks.__path__), key=operator.itemgetter(1)
+    ):
+        if modname.startswith("bm_"):
+            bench_module = importlib.import_module("benchmarks." + modname)
             for func_name in sorted(dir(bench_module)):
-                if func_name.startswith('bench_'):
+                if func_name.startswith("bench_"):
                     yield getattr(bench_module, func_name)
 
 
 def run():
     metadata = {}
-    if 'COMMIT_TIMESTAMP' in os.environ:
-        metadata['timestamp'] = os.environ.get('COMMIT_TIMESTAMP')
-        metadata['revision'] = os.environ.get('COMMIT_SHA')
-        metadata['commit_message'] = os.environ.get('COMMIT_MESSAGE').split('\n')[0]
-    runner = perf.Runner(metadata=metadata)
+    if "COMMIT_TIMESTAMP" in os.environ:
+        metadata["timestamp"] = os.environ.get("COMMIT_TIMESTAMP")
+        metadata["revision"] = os.environ.get("COMMIT_SHA")
+        metadata["commit_message"] = os.environ.get("COMMIT_MESSAGE").split("\n")[0]
+    runner = pyperf.Runner(metadata=metadata)
 
     args = runner.parse_args()
     if args.tracemalloc:
-        bench_type = 'tracemalloc'
+        bench_type = "tracemalloc"
     elif args.track_memory:
-        bench_type = 'trackmem'
+        bench_type = "trackmem"
     else:
-        bench_type = 'time'
+        bench_type = "time"
     for func in discover_benchmarks():
-        name = '%s.%s.%s' % (str(func.__module__), func.__name__, bench_type)
+        name = "%s.%s.%s" % (str(func.__module__), func.__name__, bench_type)
         runner.bench_func(name, func)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
--- run_bench_commits.py
+++ run_bench_commits.py
@@ -3,8 +3,9 @@ import shutil
 import subprocess
 
 import click
+
 import elasticsearch
-import perf
+import pyperf
 
 try:
     from urllib.request import Request, urlopen
@@ -13,13 +14,15 @@ except ImportError:
     from urllib2 import Request, urlopen
     from urllib.parse import urlparse
 
-BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', ))
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 
 def get_commit_list(start_commit, end_commit, worktree):
-    commit_range = '%s..%s' % (start_commit, end_commit)
-    commits = subprocess.check_output(['git', 'log', "--pretty=%h", commit_range], cwd=worktree).decode('utf8')
-    commits = commits.split('\n')[:-1]
+    commit_range = "%s..%s" % (start_commit, end_commit)
+    commits = subprocess.check_output(
+        ["git", "log", "--pretty=%h", commit_range], cwd=worktree
+    ).decode("utf8")
+    commits = commits.split("\n")[:-1]
     commits.reverse()
     return commits
 
@@ -27,8 +30,8 @@ def get_commit_list(start_commit, end_commit, worktree):
 def run_benchmark(commit_hash, worktree):
     # clean up from former runs
     if commit_hash:
-        subprocess.check_output(['git', 'checkout', 'elasticapm/base.py'], cwd=worktree)
-        subprocess.check_output(['git', 'checkout', commit_hash], cwd=worktree)
+        subprocess.check_output(["git", "checkout", "elasticapm/base.py"], cwd=worktree)
+        subprocess.check_output(["git", "checkout", commit_hash], cwd=worktree)
         # set the timer thread to daemon, this fixes an issue with the timer thread
         # not exiting in old commits
         subprocess.check_output(
@@ -37,32 +40,38 @@ def run_benchmark(commit_hash, worktree):
             cwd=worktree,
         )
     env = dict(**os.environ)
-    env['PYTHONPATH'] = worktree
-    env['COMMIT_TIMESTAMP'], env['COMMIT_SHA'], env['COMMIT_MESSAGE'] = subprocess.check_output(
-        ['git', 'log', '-1' ,'--pretty=%aI\t%H\t%B'],
-        cwd=worktree,
-    ).decode('utf8').split('\t', 2)
+    env["PYTHONPATH"] = worktree
+    env["COMMIT_TIMESTAMP"], env["COMMIT_SHA"], env["COMMIT_MESSAGE"] = (
+        subprocess.check_output(
+            ["git", "log", "-1", "--pretty=%aI\t%H\t%B"], cwd=worktree
+        )
+        .decode("utf8")
+        .split("\t", 2)
+    )
     output_files = []
-    for bench_type, flag in (('time', None), ('tracemalloc', '--tracemalloc')):
-        output_file = 'result.%s.%s.json' % (bench_type, commit_hash)
-        test_cmd = ['python', 'run_bench.py', '-o', output_file, '--inherit-environ', 'COMMIT_TIMESTAMP,COMMIT_SHA,COMMIT_MESSAGE,COMMIT_MESSAGE,PYTHONPATH']
+    for bench_type, flag in (("time", None), ("tracemalloc", "--tracemalloc")):
+        output_file = "result.%s.%s.json" % (bench_type, commit_hash)
+        test_cmd = [
+            "python",
+            "run_bench.py",
+            "-o",
+            output_file,
+            "--inherit-environ",
+            "COMMIT_TIMESTAMP,COMMIT_SHA,COMMIT_MESSAGE,COMMIT_MESSAGE,PYTHONPATH",
+        ]
         if flag:
             test_cmd.append(flag)
-        print(subprocess.check_output(
-            test_cmd,
-            stderr=subprocess.STDOUT,
-            env=env,
-        ))
+        print(subprocess.check_output(test_cmd, stderr=subprocess.STDOUT, env=env))
         output_files.append(output_file)
     if commit_hash:
-        subprocess.check_output(['git', 'checkout', 'elasticapm/base.py'], cwd=worktree)
+        subprocess.check_output(["git", "checkout", "elasticapm/base.py"], cwd=worktree)
     return output_files
 
 
 def upload_benchmark(es_url, es_user, es_password, files):
-    if '@' not in es_url and es_user:
+    if "@" not in es_url and es_user:
         parts = urlparse(es_url)
-        es_url = '%s://%s:%s@%s%s' % (
+        es_url = "%s://%s:%s@%s%s" % (
             parts.scheme,
             es_user,
             es_password,
@@ -72,7 +81,7 @@ def upload_benchmark(es_url, es_user, es_password, files):
     es = elasticsearch.Elasticsearch([es_url])
     result = []
     for file in files:
-        suite = perf.BenchmarkSuite.load(file)
+        suite = pyperf.BenchmarkSuite.load(file)
         for bench in suite:
             ncalibration_runs = sum(run._is_calibration() for run in bench._runs)
             nrun = bench.get_nrun()
@@ -80,51 +89,67 @@ def upload_benchmark(es_url, es_user, es_password, files):
             inner_loops = bench.get_inner_loops()
             total_loops = loops * inner_loops
             meta = bench.get_metadata()
-            meta['start_date'] = bench.get_dates()[0].isoformat(' ')
+            meta["start_date"] = bench.get_dates()[0].isoformat(" ")
             output = {
-                '_index': 'benchmark-agent-python-' + meta['timestamp'].split('T')[0],
-                '@timestamp': meta.pop('timestamp'),
-                'benchmark': meta.pop('name'),
-                'meta': meta,
-                'runs': {
-                    'calibration': ncalibration_runs,
-                    'with_values': nrun - ncalibration_runs,
-                    'total': nrun,
+                "_index": "benchmark-agent-python-" + meta["timestamp"].split("T")[0],
+                "@timestamp": meta.pop("timestamp"),
+                "benchmark": meta.pop("name"),
+                "meta": meta,
+                "runs": {
+                    "calibration": ncalibration_runs,
+                    "with_values": nrun - ncalibration_runs,
+                    "total": nrun,
                 },
-                'warmups_per_run': bench._get_nwarmup(),
-                'values_per_run': bench._get_nvalue_per_run(),
-                'median': bench.median(),
-                'median_abs_dev': bench.median_abs_dev(),
-                'mean': bench.mean(),
-                'mean_std_dev': bench.stdev(),
-                'primaryMetric': {
-                    'score': bench.mean(),
-                    'stdev': bench.stdev(),
-                    'scorePercentiles': {},
+                "warmups_per_run": bench._get_nwarmup(),
+                "values_per_run": bench._get_nvalue_per_run(),
+                "median": bench.median(),
+                "median_abs_dev": bench.median_abs_dev(),
+                "mean": bench.mean(),
+                "mean_std_dev": bench.stdev(),
+                "primaryMetric": {
+                    "score": bench.mean(),
+                    "stdev": bench.stdev(),
+                    "scorePercentiles": {},
                 },
             }
             for p in (0, 5, 25, 50, 75, 95, 100):
-                output['primaryMetric']['scorePercentiles']['%.1f' % p] = bench.percentile(p)
+                output["primaryMetric"]["scorePercentiles"][
+                    "%.1f" % p
+                ] = bench.percentile(p)
             result.append(output)
     for b in result:
-        es.index(doc_type='doc', body=b, index=b.pop('_index'))
+        es.index(doc_type="doc", body=b, index=b.pop("_index"))
 
 
 @click.command()
-@click.option('--worktree', required=True, type=click.Path(),
-              help='worktree of elastic-apm to run benchmarks in')
-@click.option('--start-commit', default=None, help='first commit to benchmark. If left empty, current worktree state will be benchmarked')
-@click.option('--end-commit', default=None, help='last commit to benchmark. If left empty, only start-commit will be benchmarked')
-@click.option('--clone-url', default=None, help='Git URL to clone')
-@click.option('--es-url', default=None, help='Elasticsearch URL')
-@click.option('--es-user', default=None, help='Elasticsearch User')
-@click.option('--es-password', default=None, help='Elasticsearch Password', envvar='ES_PASSWORD')
+@click.option(
+    "--worktree",
+    required=True,
+    type=click.Path(),
+    help="worktree of elastic-apm to run benchmarks in",
+)
+@click.option(
+    "--start-commit",
+    default=None,
+    help="first commit to benchmark. If left empty, current worktree state will be benchmarked",
+)
+@click.option(
+    "--end-commit",
+    default=None,
+    help="last commit to benchmark. If left empty, only start-commit will be benchmarked",
+)
+@click.option("--clone-url", default=None, help="Git URL to clone")
+@click.option("--es-url", default=None, help="Elasticsearch URL")
+@click.option("--es-user", default=None, help="Elasticsearch User")
+@click.option(
+    "--es-password", default=None, help="Elasticsearch Password", envvar="ES_PASSWORD"
+)
 def run(worktree, start_commit, end_commit, clone_url, es_url, es_user, es_password):
     cloned = False
     if clone_url:
         if os.path.exists(worktree):
             raise click.UsageError("%s exists, can't clone" % worktree)
-        subprocess.check_output(['git', 'clone', clone_url, worktree])
+        subprocess.check_output(["git", "clone", clone_url, worktree])
         cloned = True
     if start_commit:
         if end_commit:
@@ -142,5 +167,5 @@ def run(worktree, start_commit, end_commit, clone_url, es_url, es_user, es_passw
         upload_benchmark(es_url, es_user, es_password, json_files)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
