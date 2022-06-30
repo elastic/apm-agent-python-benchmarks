854ff60d726d16c1b57d5df94a79a364ab83cdd1
Benjamin Wohlwend <beni@elastic.co>
2022-06-30 16:45:33 +0200

    add some rust benches

--- benchmarks/bm_exceptions.py
+++ benchmarks/bm_exceptions.py
@@ -2,9 +2,9 @@ import elasticapm
 from benchmarks.decorators import with_elasticapm_client
 
 
-@with_elasticapm_client()
-def bench_capture_exception(client):
-    try:
-        assert False
-    except AssertionError:
-        id = client.capture_exception()
+# @with_elasticapm_client()
+# def bench_capture_exception(client):
+#     try:
+#         assert False
+#     except AssertionError:
+#         id = client.capture_exception()
--- benchmarks/decorators.py
+++ benchmarks/decorators.py
@@ -5,6 +5,7 @@ class with_elasticapm_client:
     def __init__(self, **client_defaults):
         client_defaults.setdefault("disable_send", True)
         client_defaults.setdefault("service_name", "benchmarks")
+        client_defaults.setdefault("server_version", "8.3.0")
         self.client_defaults = client_defaults
 
     def __call__(self, f):
